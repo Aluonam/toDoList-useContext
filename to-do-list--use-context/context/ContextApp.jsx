066ca2b33cac3e4a854513cac6e0e59225a542f7
Paula Cano <paulacanoroman@gmail.com>
2023-09-21 10:04:25 +0200

    feat: estructura de useContext comentada

--- to-do-list--use-context/context/ContextApp.jsx
+++ to-do-list--use-context/context/ContextApp.jsx
@@ -4,13 +4,21 @@ export const ToDoListContext = createContext();
 
 export const ContextApp = (props) => {
 
+//---------------variables/const/hooks-------------------------
+    const [taskList, setTaskList] = useState([])
 
 
+
+//---------------objeto valuesProvider (especifico qué se va a compartir)-------------------------
+
+
+
+//---------------envoltura de children e indica los valores a los que tendrán acceso value={valuesProvider}-------------------------
   return (
     <>
-    <ToDoListContext.Provider>
+    <ToDoListContext.Provider  > 
 
-        
+
     </ToDoListContext.Provider>
     </>
   )
